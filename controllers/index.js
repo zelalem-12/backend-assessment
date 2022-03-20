39e9594a5a37381a263913a6082f623f9eca4952
zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-03-20 19:13:43 +0300

    fix on search query validator on controller

--- controllers/index.js
+++ controllers/index.js
@@ -2,21 +2,23 @@ const Topics = require("../models/topics");
 
 async function getSubTopicQuestions(req, res) {
   const topicName = req.query.q;
-
-  if (
-    !topicName &&
-    typeof topicName !== "string" &&
-    !topicName.trim() &&
-    !topicName.replaceAll(/\r?\n|\r/g, "")
-  ) {
-    return res.status(400).send({ message: "Missing topic search query!" });
-  }
   try {
+    if (
+      !topicName ||
+      typeof topicName !== "string" ||
+      !topicName.trim() ||
+      !topicName.replaceAll(/\r?\n|\r/g, "")
+    ) {
+      return res.status(400).send({ message: "Missing topic search query!" });
+    }
+
     const queryTopic = topicName.replaceAll(/\s+/g, " ").trim();
     const subTopicQUestions = await Topics.getTopics(queryTopic);
     res.send(subTopicQUestions);
   } catch (err) {
-    res.status(err.status || 500).send({});
+    res
+      .status(err.status || 500)
+      .send({ message: err.message || "Server Error " });
   }
 }
 
